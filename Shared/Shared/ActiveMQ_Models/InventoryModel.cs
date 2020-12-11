70b04fafa6bb5024c1926bf5356afbfe42a9e9c5
Matthijs Kotterink <poukimonster@gmail.com>
2020-12-11 14:51:32 +0100

    added shared folder

---
+++ Shared/Shared/ActiveMQ_Models/InventoryModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shared.Shared.ActiveMQ_Models
+{
+	public class InventoryModel
+	{ 
+		public Guid id { get; set; }
+		public Guid userId { get; set; }
+		public List<Guid> itemId { get; set; }
+	}
+}
