76b4e0b045abb03a14d411f3fa7f657855982b94
Matthijs Kotterink <poukimonster@gmail.com>
2021-01-14 14:33:15 +0100

    nuget update

--- Shared/Shared/ApiModels/AnimalModel.cs
+++ Shared/Shared/ApiModels/AnimalModel.cs
@@ -4,19 +4,19 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace Shared.Shared.ApiModels
 {
-    [ExcludeFromCodeCoverage]
-    public class AnimalModel
+	[ExcludeFromCodeCoverage]
+	public class AnimalModel
 	{
-        [BsonId]
-        public Guid Id { get; set; }
-        public Guid UserId { get; set; }
+		[BsonId]
+		public Guid Id { get; set; }
+		public Guid UserId { get; set; }
 
-        public string Name { get; set; }
+		public string Name { get; set; }
 
-        public float Food { get; set; }
-        public float Energy { get; set; }
-        public float Happiness { get; set; }
+		public float Food { get; set; }
+		public float Energy { get; set; }
+		public float Happiness { get; set; }
 
-        public int AnimalType { get; set; }
-    }
+		public int AnimalType { get; set; }
+	}
 }
